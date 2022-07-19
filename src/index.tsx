bbb8685532f1d49b9ae7aff3c18f0e5399d35f69
EwanMcA <ewan.m@hotmail.com>
2022-07-19 22:41:29 +0100

    Add sass and the first component

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,10 @@ import React from 'react';
 
 import { createRoot } from 'react-dom/client';
 
-const root = createRoot(document.getElementById('index'));
-root.render(<h1>Ewan, Dev</h1>);
+import Landing from './landing';
+
+const index = document.getElementById('index');
+if (index) {
+  const root = createRoot(index);
+  root.render(<Landing />);
+}
---
+++ src/landing/landing.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './landing.scss';
+
+const Landing = (): JSX.Element => (
+  <main className={styles['landing-main']}>
+    <h1>Ewan, Dev</h1>
+  </main>
+);
+
+export default Landing;
