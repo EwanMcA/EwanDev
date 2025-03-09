b22f6958f2b892acbcdbb17d58f81ee1eba52387
Ewan <ewan.m@hotmail.com>
2025-03-09 17:07:19 +0000

    Try ink drip animation

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -1,28 +1,43 @@
-import React, { VFC } from 'react';
+import React, { useEffect, VFC } from 'react';
+import { gsap } from "gsap";
 
 import { Link } from 'react-router-dom';
 
-//import inkGif from '../home/inkDrip.gif';
+import inkGif from '../drip.gif';
 import styles from './home.scss';
-import profilePic from '../header/profileInk.png';
+import ProfilePic from '../profileInk';
 
-const Home: VFC = () => (
-  <main className={styles.homepage}>
-    <img className={styles.profile} src={profilePic} />
-    <nav className={styles.menu}>
-      <ul>
-        <li>
-          <Link to="about">About</Link>
-        </li>
-        <li>
-          <Link to="projects">Projects</Link>
-        </li>
-        <li>
-          <Link to="contact">Contact</Link>
-        </li>
-      </ul>
-    </nav>
-  </main>
-);
+const Home: VFC = () => {
+  useEffect(() => {
+    gsap.to(".fade-element", {
+      opacity: 0,
+      duration: 5,
+      ease: "power2.inOut",
+      stagger: 0.01,
+    });
+  }, []);
+
+  return (
+    <main className={styles.homepage}>
+      <div className={styles.images}>
+        <ProfilePic />
+        <img src={inkGif} alt="Ink drip" height={200} width={100} className={["fade-element", styles.drip].join(" ")}/>
+      </div>
+      <nav className={styles.menu}>
+        <ul>
+          <li>
+            <Link to="about">About</Link>
+          </li>
+          <li>
+            <Link to="projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </main>
+  )
+};
 
 export default Home;
